0a1a6b3b495d0f7907cd2872a2b3721442643f35
Umwelt32 <56482176+Umwelt32@users.noreply.github.com>
2022-09-26 15:43:29 +0200

    Add files via upload

--- Algos/Python/MDBU_python/main.py
+++ Algos/Python/MDBU_python/main.py
@@ -14,8 +14,8 @@ def run_mdbu_sw(seed,h,i,file_in,file_out):
     params = ['ds1.exe','-h',str(h),'-i',str(i),'-seed',str(seed),'-file',str(file_in),'-out',str(file_out)]
     subprocess.run(params)
     
-def parse_file(fileName):
-    f1 = fileName.replace('.','_')
+def parse_file(fileName,out):
+    f1 = out+fileName.replace('.','_')
     f1_in = f1+'.bmp'
     f1_in_canny = f1+'_canny.bmp'
     f1_in_mdbu = f1+'_mdbu.bmp'
@@ -26,13 +26,13 @@ def parse_file(fileName):
 def convHgtToBmpRecurse(in_path):
      list = [f for f in os.listdir(in_path) if f.endswith('.hgt')]
      for file in list:
-        parse_file(file)
+        parse_file(file,'./out/')
 
 if __name__ == "__main__":
     if len(sys.argv)>1:
         fileName=str(sys.argv[1])
         print('Open File: '+fileName+'...')
-        parse_file(fileName)
+        parse_file(fileName,'./out/')
         exit(0)
     else:
         print("Missing input argument! - path to .hgt file.")
