44cda5fcedca12107cff9852c07dab1a77624559
Umwelt32 <56482176+Umwelt32@users.noreply.github.com>
2022-10-21 20:34:29 +0200

    Add files via upload

---
+++ Algos/Python/tibia_spr/tibia_spr2bmp.py
@@ -0,0 +1,12 @@
+##################################################
+## Author: https://github.com/Umwelt32/QuickCode
+## Copyright: 2022
+## Convert .spr file's to .bmp format
+## References:
+##############
+##############
+##################################################
+
+import cv2,os,sys,numpy,math
+
+
