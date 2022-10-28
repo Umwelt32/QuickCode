9a8d3e47c610ed1250a7d958dcada2edb42c5eb9
Umwelt32 <56482176+Umwelt32@users.noreply.github.com>
2022-10-28 22:09:53 +0200

    Add files via upload

--- Algos/Python/tibia_spr/tibia_spr2bmp.py
+++ Algos/Python/tibia_spr/tibia_spr2bmp.py
@@ -12,20 +12,34 @@ import cv2,os,sys,numpy,math
 m_file_ver        = None
 m_sprites_count   = None
 m_sprites_offsets = None
+m_file_handle     = None
+m_sprites         = None
 
 def load_file(path):
     global m_file_ver
     global m_sprites_count
     global m_sprites_offsets
-    dt_header = numpy.dtype({'names': ['r','g','b','a'],'formats': [numpy.uint8, numpy.uint8, numpy.uint8, numpy.uint8]})
-    f = open(path, "rb")
-    m_file_ver      = numpy.fromfile(f, dtype=numpy.uint32,count=1)[0]
-    m_sprites_count = numpy.fromfile(f, dtype=numpy.uint16,count=1)[0]
-    print("ver: "+str(m_file_ver)+"\nsprites_count: "+str(m_sprites_count))
-    m_sprites_offsets = numpy.fromfile(f, dtype=numpy.uint32,count=m_sprites_count)
-    f.close()
+    global m_file_handle
+    global m_sprites
+    m_file_handle     = open(path, "rb")
+    m_file_ver        = numpy.fromfile(m_file_handle, dtype=numpy.uint32,count=1)[0]
+    m_sprites_count   = numpy.fromfile(m_file_handle, dtype=numpy.uint16,count=1)[0]
+    m_sprites_offsets = numpy.fromfile(m_file_handle, dtype=numpy.uint32,count=m_sprites_count)
+    m_sprites         = [load_sprite(offset) for offset in m_sprites_offsets]
 
-
+def load_sprite(sprite_idx):
+    global m_file_handle
+    global m_sprites_offsets
+    if sprite_idx  != None: m_file_handle.seek(m_sprites_offsets[sprite_idx], 0)
+    transparent_key = numpy.fromfile(m_file_handle, dtype=numpy.uint8, count=3)
+    sprite_size     = numpy.fromfile(m_file_handle, dtype=numpy.uint16,count=1)[0]
+    t_pixels_number = numpy.fromfile(m_file_handle, dtype=numpy.uint16,count=1)[0]
+    c_pixels_number = numpy.fromfile(m_file_handle, dtype=numpy.uint16,count=1)[0]
+    sprite_data     = numpy.fromfile(m_file_handle, dtype=numpy.uint8, count=sprite_size)
+    total_pixels    = t_pixels_number+c_pixels_number
+    sprite_n        = math.floor(math.sqrt(total_pixels))
+    package = {'transparent_key':transparent_key,'sprite_size':sprite_size,'total_pixels':total_pixels,'sprite_n':sprite_n,'sprite_data':sprite_data}
+    return package
 
 if __name__ == "__main__":
     load_file('Tibia.spr')
