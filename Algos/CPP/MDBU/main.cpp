6da13179afb3b5729d24fffac188e898cee6fe4d
Umwelt32 <56482176+Umwelt32@users.noreply.github.com>
2023-03-25 20:27:12 +0100

    Update main.cpp

--- Algos/CPP/MDBU/main.cpp
+++ Algos/CPP/MDBU/main.cpp
@@ -1,30 +1,18 @@
-      /*---------------------------------------------------------*/
-     /* Author: https://github.com/Umwelt32/QuickCode           */
-    /* Copyright: 2022                                         */
-   /* MDBU algorithm.                                         */
-  /* References:                                             */
- /* https://doi.org/10.1145/1294685.1294717                 */
-/*---------------------------------------------------------*/
-
-#include <iostream>
-#include "ds_mdbu.hpp"
-#include "utils.hpp"
-#include "algo_utils.hpp"
+#include "ca_pcg.hpp"
 
 using namespace std;
+
 static U32 paramnr=0;
 static U32 param_count=0;
 static char **params=nullptr;
-std::string m_input_file="bitmap.bmp";
+static U32 m_seed = 128u;
+static U32 m_w = 256u;
+static U32 m_h = 256u;
+static U32 m_n = 16u;
+static U32 m_m = 2u;
+static U32 m_t = 16u;
+static F32 m_r = 0.75f;
 std::string m_output_file="bitmap_out.bmp";
-U32 m_seed = 128u;
-F32 m_h = 0.3f;
-F32 m_bmp_th = 0.01f;
-int m_n=0;
-bool m_mdbu = true;
-bool m_recurse = false;
-bool m_gen_all_out = false;
-bool m_extend = false;
 
 bool isParamToRead(void){return (paramnr<param_count);}
 std::string read_param_str(void){if(isParamToRead()){return std::string(params[paramnr++]);}else{return "";}}
@@ -35,16 +23,13 @@ void help_view(void)
 {
     std::cout<<"-----------"<<std::endl;
     std::cout<<"-seed <int>"<<std::endl;
-    std::cout<<"-h <float>"<<std::endl;
-    std::cout<<"-file <file>"<<std::endl;
-    std::cout<<"-th <float>"<<std::endl;
     std::cout<<"-out <file>"<<std::endl;
     std::cout<<"-n <int>"<<std::endl;
-    std::cout<<"-i <int>"<<std::endl;
-    std::cout<<"-all_out"<<std::endl;
-    std::cout<<"-e"<<std::endl;
-    std::cout<<"-r"<<std::endl;
-    std::cout<<"-dis_mdbu"<<std::endl;
+    std::cout<<"-r <float>"<<std::endl;
+    std::cout<<"-t <int>"<<std::endl;
+    std::cout<<"-m <int>"<<std::endl;
+    std::cout<<"-w <int>"<<std::endl;
+    std::cout<<"-h <int>"<<std::endl;
     std::cout<<"-help"<<std::endl;
     std::cout<<"-----------"<<std::endl;
 }
@@ -59,44 +44,22 @@ void parse_params(int argc, char **argv)
         std::string param = read_param_str();
         std::cout<<param<<" ";
         if (param=="-seed"){m_seed=read_param_int();}
-        else if(param=="-h"){m_h=read_param_f();}
-        else if(param=="-th"){m_bmp_th=read_param_f();}
-        else if(param=="-file"){m_input_file=read_param_str();}
         else if(param=="-out"){m_output_file=read_param_str();}
+        else if(param=="-w"){m_w=read_param_int();}
+        else if(param=="-h"){m_h=read_param_int();}
+        else if(param=="-r"){m_r=read_param_f();}
         else if(param=="-n"){m_n=read_param_int();}
-        else if(param=="-i"){algo::utils::setIConst(read_param_int());}
-        else if(param=="-all_out"){m_gen_all_out=true;}
-        else if(param=="-e"){m_extend=true;}
-        else if(param=="-r"){m_recurse=true;}
-        else if(param=="-dis_mdbu"){m_mdbu=false;}
+        else if(param=="-m"){m_m=read_param_int();}
+        else if(param=="-t"){m_t=read_param_int();}
         else if(param=="-help"){help_view();exit(0);}
     }
 }
 
 int main(int argc, char **argv)
 {
-    parse_params(argc,argv);
-    algo::array2d<F32> bmp;
-    algo::array2d<F32> o[3];
-    algo::ds_mdbu w;
-    if (m_n<=0)
-    {
-        algo::utils::loadBmpFile(bmp,m_input_file);
-        w.setInitialPoints(bmp,m_bmp_th,m_extend);
-    }
-    else
-    {
-        w.init(m_n);
-    }
-    w.do_ds(m_seed,m_mdbu,m_recurse,m_h,&o[0],&o[1],&o[2]);
-    algo::utils::normalizeArray(o[2]);
-    algo::utils::normalizeArray(o[1]);
-    if(m_gen_all_out)
-    {
-        algo::utils::saveBmpFile(o[0],m_output_file+"_in_.bmp");
-        algo::utils::saveBmpFile(o[1],m_output_file+"_e_.bmp");
-        algo::utils::saveTxtFile(o[2],m_output_file+"_out.txt");
-    }
-    algo::utils::saveBmpFile(o[2],m_output_file);
+    ca_pcg node;
+    node.init(m_w,m_h);
+    node.generate(m_seed,m_r,m_n,m_t,m_m);
+    node.saveToBmp(m_output_file.c_str());
     return 0;
 }
