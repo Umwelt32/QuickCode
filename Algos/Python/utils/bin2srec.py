e97d9f259607a3775e92b10961e5dbe1b675c6a4
Umwelt32 <56482176+Umwelt32@users.noreply.github.com>
2023-03-27 20:53:10 +0200

    Update bin2srec.py

--- Algos/Python/utils/bin2srec.py
+++ Algos/Python/utils/bin2srec.py
@@ -1,11 +1,19 @@
 import os,sys,math,numpy
 
-def srec_bin2hex(path,addr_offset,line_size):
-    output_data = ['S00F000068656C6C6F202020202000003C']
-    file_data   = numpy.fromfile(str(path),dtype=numpy.ubyte)
-    #file_data   = numpy.fromfile(str(path),dtype=numpy.ubyte, count=-1, offset=0, *, like=None)
+def srec_bin2hex(path,addr_offset=0x0000,line_size=32,addr_width=2,file_offset=0):
+    header_data = 'S00F000068656C6C6F202020202000003C'
+    file_data   = numpy.fromfile(str(path),offset=file_offset,dtype=numpy.ubyte)
     line_size   = min(line_size,0xFF)
     line_size   = max(line_size,0x00)
+    file_chunks = numpy.split(file_data, line_size)
+    file_chunks_count = len(file_chunks)
+    srec_lines  = [str(_srec_get_srec_reg(addr_width,addr_offset+(idx*line_size),file_chunks[idx])) for idx in range(file_chunks_count)]
+    srec_count  = _srec_gen_count_reg(file_chunks_count)
+    srec_terminator  = _srec_gen_terminator_reg(addr_width)
+    output_data      = [header_data]
+    for line in srec_lines: output_data.append(line)
+    output_data.append(srec_count)
+    output_data.append(srec_terminator)
     return output_data
 
 def _srec_get_srec_reg(addr_width,base_addr,data):
@@ -32,7 +40,7 @@ def _srec_calc_checksum(data_array):
     checksum_u8    = numpy.uint8(0xFF - numpy.uint8(numpy.bitwise_and(checksum_u16,0xFF)))
     return checksum_u8
     
-def srec_gen_count_reg(lines_count):
+def _srec_gen_count_reg(lines_count):
     header = ['S5','S5','S5','S6','S6','S6']
     lines_count_u32 = numpy.uint32(lines_count)
     size_bytes = 0x02 if lines_count_u32>0xFFFF else 0x03
@@ -45,7 +53,7 @@ def srec_gen_count_reg(lines_count):
     srec_complete   = str(header[addr_width])+''+str(_srec_array2string(complement_data))
     return srec_complete
 
-def srec_gen_terminator_reg(addr_width):
+def _srec_gen_terminator_reg(addr_width):
     header = ['S9','S9','S9','S8','S7','S7']
     addr_width  = min(addr_width,0x04)
     addr_width  = max(addr_width,0x02)
