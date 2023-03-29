92fc1713a31e968d7b7033f8fa2bc35a8f15ff32
Umwelt32 <56482176+Umwelt32@users.noreply.github.com>
2023-03-29 11:13:10 +0200

    Add files via upload

--- Algos/Python/utils/bin2chunks.py
+++ Algos/Python/utils/bin2chunks.py
@@ -9,13 +9,17 @@
 
 import os,sys,math,numpy
 
+m_file_size = 0;
+
 def bin2rec_save_file(path,out_dir='./bin_chunks',list_out_file='',chunk_size=0x40000):
+    global m_file_size
     result=True
     try:
         print('input file: '+str(path))
         print('output directory: '+str(out_dir))
         list_file     = list_out_file if len(list_out_file)>0 else path.replace('\\','/').split('/')[-1].replace('.','_')+'.txt'
         file_data     = numpy.fromfile(str(path),dtype=numpy.ubyte)
+        m_file_size   = len(file_data)
         output_chunks = _bin2rec_split2chunks(file_data,chunk_size)
         _bin2rec_save_chunks(out_dir+'/'+list_file,out_dir,output_chunks,chunk_size)
     except:
@@ -42,18 +46,21 @@ def _bin2rec_save_txt_file(path,data):
     f.close()
 
 def _bin2rec_save_chunks(list_file_name,directory,chunks_data,chunk_size):
+    global m_file_size
     files2addr = []
     for idx in range(len(chunks_data)): 
         filename = str(idx)+'.BIN'
         _bin2rec_save_bin_file(directory+'/'+filename,chunks_data[idx])
         files2addr.append(str(hex(idx*chunk_size))+';'+filename)
     files2addr.insert(0,str(hex(len(files2addr))))
+    files2addr.insert(0,str(hex(m_file_size)))
+    files2addr.insert(0,str(hex(chunk_size)))
     _bin2rec_save_txt_file(list_file_name,files2addr)
 
 if __name__ == "__main__":
     if (len(sys.argv) > 1):
         input_file_path = str(sys.argv[1])
-        result = bin2rec_save_file(input_file_path,'','./bin_chunks',0x40000)
+        result = bin2rec_save_file(input_file_path,str(sys.argv[2]),str(sys.argv[3]),0x40000)
         exit(0x00) if result==True else exit(0x01)
     else:
         print('no input file in script parameter!')
