61fad6b8f80aee86a00c0b23666c5d6cc59bb609
Umwelt32 <56482176+Umwelt32@users.noreply.github.com>
2023-04-04 08:15:53 +0200

    Update perlin.py

--- Algos/Python/Perlin/perlin.py
+++ Algos/Python/Perlin/perlin.py
@@ -23,19 +23,19 @@ def _float2byte(array):
     img_byte[:,:]  = ((img_int[:,:]-_min)/(_base))*255
     return img_byte
 
-def perlin_noise_gen(w,h,_seed,_octaves,lac):
+def perlin_noise_gen(w,h,_seed,_octaves,lac,ridge):
     img      = numpy.zeros(shape=(w,h), dtype=numpy.float32)
     noise    = perlin_noise.PerlinNoise(seed=_seed,octaves=1)
     for i in range(w):
         for j in range(h):
             val = 0
             for p in range(0,_octaves,1):
-                f   = math.pow(lac,p)
-                val = val+(math.pow(2,-p) * noise([_fdiv(i*f,w),_fdiv(j*f,h)]))
-            img[i,j]=val
+                f      = math.pow(lac,p)
+                sample = (math.pow(2,-p) * noise([_fdiv(i*f,w),_fdiv(j*f,h)]))
+                val    = (val+math.fabs(sample)) if ridge==True else (val+sample)
+            img[i,j]   = val
     return _float2byte(img)
-    
 
 if __name__ == "__main__":
-    img = perlin_noise_gen(512,512,128,8,3)
+    img = perlin_noise_gen(512,512,128,8,3,False)
     cv2.imwrite('noise.bmp', img)
