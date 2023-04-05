bb2b6e1a8a2cae7cba980c327993ed95a1467127
Umwelt32 <56482176+Umwelt32@users.noreply.github.com>
2023-04-05 09:43:23 +0200

    Update simpleds.py

--- Algos/Python/DiamondSquare/simpleds.py
+++ Algos/Python/DiamondSquare/simpleds.py
@@ -8,7 +8,7 @@
 
 import os,sys,math,numpy,random,cv2
 
-m_params = {"data": None, "n": 0,"size": 0,"level": 0,"current_half": 0}
+m_params = {"data": None,"size": 0,"h_factor": 0,"current_half": 0,"current_size": 0,"current_ratio": 0}
 
 def set_random_seed(seed):
     random.seed(int(seed))
@@ -35,44 +35,48 @@ def get_point_value(pos):
     else:
         return value
 
-def generateDsMap(n):
+def generateDsMap(n,h):
     global m_params
-    m_params['level']        = int(0)
-    m_params['n']            = int(n)
-    m_params['size']         = int(math.floor(math.pow(2,m_params['n'])+1))
-    m_params['current_half'] = int(math.floor(m_params['size']/2))
-    m_params['data']         = numpy.zeros(shape=(m_params['size'],m_params['size']), dtype=numpy.float32)
-    m_params['data'][0,  0]  = get_random_uniform(1.0)
-    m_params['data'][-1, 0]  = get_random_uniform(1.0)
-    m_params['data'][0, -1]  = get_random_uniform(1.0)
-    m_params['data'][-1,-1]  = get_random_uniform(1.0)
+    m_params['h_factor']      = float(math.pow(2,-h))
+    m_params['size']          = int(math.floor(math.pow(2,int(n))+1))
+    m_params['current_ratio'] = float(1)
+    m_params['current_half']  = int(math.floor(m_params['size']/2))
+    m_params['current_size']  = int(m_params['size'])
+    m_params['data']          = numpy.zeros(shape=(m_params['size'],m_params['size']), dtype=numpy.float32)
+    m_params['data'][0,  0]   = get_random_uniform(1.0)
+    m_params['data'][-1, 0]   = get_random_uniform(1.0)
+    m_params['data'][0, -1]   = get_random_uniform(1.0)
+    m_params['data'][-1,-1]   = get_random_uniform(1.0)
 
-def calculate_mid(x,y,half,level,diamond):
+def calculate_mid(x,y,half,ratio,diamond):
     global m_params
     points_d_xy = [[x-half,y-half],[x+half,y+half],[x-half,y+half],[x+half,y-half]]
     points_s_xy = [[x,y-half],[x,y+half],[x+half,y],[x-half,y]]
     points_xy   = points_d_xy if diamond==True else points_s_xy
     value_list  = [get_point_value(p) for p in points_xy if get_point_value(p)!=None]
-    point_value = numpy.average(value_list)+get_random_uniform(math.pow(2,-level))
+    point_value = numpy.average(value_list)+get_random_uniform(float(ratio))
     set_point_value([x,y],point_value)
 
 def do_iteration():
     global m_params
+    size  = m_params['size']
     while  m_params['current_half'] > 0:
-        size   = m_params['size']
-        half   = m_params['current_half']
-        _2half = int(half*2)
-        level = m_params['level']
-        m_params['level']=level+1
-        print(m_params['current_half'])
-        m_params['current_half']=int(math.floor(half/2))
-        for i in range(0,size,half):
-            for j in range(0,size,half):
-                calculate_mid(i+half,j+half,      half,level,True )
-                calculate_mid(i+half,j,           half,level,False)
-                calculate_mid(i,j+half,           half,level,False)
-                calculate_mid(i+_2half,j+half,  half,level,False)
-                calculate_mid(i+half,  j+_2half,half,level,False)
+        half         = m_params['current_half']
+        current_size = m_params['current_size']
+        ratio = m_params['current_ratio']
+        m_params['current_ratio'] = float(m_params['current_ratio']*m_params['h_factor'])
+        m_params['current_size']  = half
+        m_params['current_half']  = int(math.floor(half/2))
+        for diamond in [True,False]:
+            for i in range(0,size,current_size):
+                for j in range(0,size,current_size):
+                    if diamond:
+                        calculate_mid(i+half,j+half,half,ratio,    True )
+                    else:
+                        calculate_mid(i+half,j,half,ratio,         False)
+                        calculate_mid(i,j+half,half,ratio,         False)
+                        calculate_mid(i+(2*half),j+half,half,ratio,False)
+                        calculate_mid(i+half,j+(2*half),half,ratio,False)
                 
 
 def _float2byte(array):
@@ -91,7 +95,7 @@ def save_img(filename):
 
 if __name__ == "__main__":
     set_random_seed(10)
-    generateDsMap(8)
+    generateDsMap(10,0.3)
     do_iteration()
     save_img('out.bmp')
     exit(0x00)
