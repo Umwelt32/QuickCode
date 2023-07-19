de056e68a79e76b0c2234c433eb6035a8a763dd0
Umwelt32 <56482176+Umwelt32@users.noreply.github.com>
2023-07-19 13:30:07 +0200

    Update tibia_spr2bmp.py

--- Algos/Python/tibia_spr/tibia_spr2bmp.py
+++ Algos/Python/tibia_spr/tibia_spr2bmp.py
@@ -44,17 +44,17 @@ def _load_sprite(sprite_idx):
 def _sprite_data2img(sprite_data,n,transparent_color):
     raw_data = numpy.zeros(n*n*3,dtype=numpy.uint8)
     data_offset= 0
-    pixel_byte = 0
+    pixel_byte_offset = 0
     while data_offset < len(sprite_data): #repeat until current sprite data ends
         try:
             pix_offsets         = numpy.frombuffer(sprite_data, dtype = numpy.uint16,count=2,offset=data_offset) #read num of transparent and color pixels, (2+2)bytes
-            color_bytes_to_read = numpy.uint16(pix_offsets[1]*3)
+            color_bytes_to_read = numpy.uint16(pix_offsets[1]*3) #calculate amount of color bytes
             color_data          = numpy.frombuffer(sprite_data, dtype = numpy.uint8,count=color_bytes_to_read,offset=(data_offset+4)) #read color pixels data
-            raw_data=_memcpy(raw_data,transparent_color*pix_offsets[0],pixel_byte) #flush transparent pixels data
-            pixel_byte=pixel_byte+(pix_offsets[0]*3)
-            raw_data=_memcpy(raw_data,color_data,pixel_byte) #flush color pixel data
-            pixel_byte=pixel_byte+color_bytes_to_read        #move over pixel buffer
-            data_offset=(data_offset+color_bytes_to_read+4)  #move over sprite buffer
+            raw_data=_memcpy(raw_data,transparent_color*pix_offsets[0],pixel_byte_offset) #flush transparent pixels data
+            pixel_byte_offset=pixel_byte_offset+(pix_offsets[0]*3)     #move over pixel buffer
+            raw_data=_memcpy(raw_data,color_data,pixel_byte_offset)    #flush color pixel data
+            pixel_byte_offset=pixel_byte_offset+color_bytes_to_read    #move over pixel buffer
+            data_offset=(data_offset+color_bytes_to_read+4)            #move over sprite buffer
         except:
             break
     return raw_data.reshape((n,n,3))
@@ -72,8 +72,10 @@ def ex_sprites_to_dir(path):
         cv2.imwrite(path+'/'+str(idx)+'.bmp', _x)
         idx=idx+1
 
-def ex_sprites_to_bmp(n,m,h,path):
+def ex_sprites_to_bmp(n,m,h,file_path):
     global m_sprites_data
+    file_path=file_path.replace('\\','/')
+    base_filename = file_path.split('/')[-1]
     blank_image = numpy.zeros((n*m, n*m, 3), dtype=numpy.uint8)
     idx=0
     file_idx=0
@@ -87,12 +89,12 @@ def ex_sprites_to_bmp(n,m,h,path):
                             y1 = y+((w)*m)
                             blank_image[x1:(x1+m),y1:(y1+m)]=m_sprites_data[idx]
                             idx=idx+1
+        cv2.imwrite(file_path.replace(base_filename,str(file_idx)+'_'+base_filename), cv2.cvtColor(blank_image, cv2.COLOR_BGR2RGB))
         file_idx=file_idx+1
-        cv2.imwrite(path+'/'+str(file_idx)+'.bmp', cv2.cvtColor(blank_image, cv2.COLOR_BGR2RGB))
         blank_image = numpy.zeros((n*m, n*m, 3), dtype=numpy.uint8)
 
 if __name__ == "__main__":
     load_file('Tibia.spr')
     #ex_sprites_to_dir('./out')
-    ex_sprites_to_bmp(128,32,1,'./out2')
+    ex_sprites_to_bmp(128,32,1,'./output.bmp')
     exit(0)
