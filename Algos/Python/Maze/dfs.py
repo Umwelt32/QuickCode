83db5525b5ebe8de2b5a8b53656ab631b8d7dd3b
Umwelt32 <56482176+Umwelt32@users.noreply.github.com>
2023-08-11 09:48:40 +0200

    Add files via upload

--- Algos/Python/Maze/dfs.py
+++ Algos/Python/Maze/dfs.py
@@ -1,24 +1,39 @@
 import cv2,os,sys,numpy,math
 
-m_maze = None
+m_maze  = None
+m_stack = None
 
-def _init(n):
+def _init(n,seed):
     global m_maze
-    m_maze = numpy.zeros((n*2, n*2), dtype=numpy.uint8)
+    global m_stack
+    numpy.random.seed(seed)
+    m_stack = [[_rnd_rand_int(0,n)*2,_rnd_rand_int(0,n)*2]]
+    m_maze  = numpy.zeros((n*2, n*2), dtype=numpy.uint8)
     for x in range(0,m_maze.shape[0],2):
         for y in range(0,m_maze.shape[0],2):
             m_maze[x][y]=2
 
-def _dfs(x,y):
-    global m_maze
-    xy   = [[x+1,y],[x-1,y],[x,y+1],[x,y-1]]
-    xy_z = [[x+2,y],[x-2,y],[x,y+2],[x,y-2]]
+def _rnd_rand_int(s,e):
+    if (s==e): return int(s)
+    return int(numpy.random.randint(s,e))
+
+def _dfs_stack():
+    global m_stack
+    while len(m_stack)>0:
+        v = m_stack.pop(0)
+        _dfs(v[0],v[1],_rnd_rand_int(1,16))
+
+def _dfs(x,y,stack):
+    global m_stack
+    xy = [[[x+1,y],[x+2,y]],[[x-1,y],[x-2,y]],[[x,y+1],[x,y+2]],[[x,y-1],[x,y-2]]]
     if _setCellValue(x,y,1)==False: return False
-    for idx in range(len(xy_z)):
-        if _getCellValue(xy_z[idx][0],xy_z[idx][1])==2:
-            _setCellValue(xy[idx][0],xy[idx][1],1)
-            _dfs(xy_z[idx][0],xy_z[idx][1])
-    
+    if stack==0: m_stack.append([x,y])
+    if stack==0: return False
+    for idx in range(len(xy)):
+        if _getCellValue(xy[idx][1][0],xy[idx][1][1])==2:
+            _setCellValue(xy[idx][0][0],xy[idx][0][1],1)
+            _dfs(xy[idx][1][0],xy[idx][1][1],stack-1)
+
 def _save_img(filename):
     global m_maze
     m_maze[m_maze==1]=255
@@ -43,7 +58,7 @@ def _setCellValue(x,y,v):
         return True
 
 if __name__ == "__main__":
-    _init(32)
-    _dfs(16,8)
+    _init(128,256)
+    _dfs_stack()
     _save_img('out.bmp')
     
