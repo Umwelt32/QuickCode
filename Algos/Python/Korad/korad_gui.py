7f9d86f9dc3773ff4599723fbea3fe80588656c0
Umwelt32 <56482176+Umwelt32@users.noreply.github.com>
2023-10-17 14:51:33 +0200

    Add files via upload

--- Algos/Python/Korad/korad_gui.py
+++ Algos/Python/Korad/korad_gui.py
@@ -22,9 +22,9 @@ m_layout = [
     [sg.Text('KORAD_DEVICE:',size=(15, 1)),sg.Text('<NOT OPENED!>',size=(25, 1), key='-l_dev_name-')],
     [sg.Text('STATUS:', size=(15, 1)),sg.Canvas(size=(25, 25), background_color='red', key='-l_dev_status_led-'),sg.Text('N/A',key='-l_dev_status-',size=(10, 1)),sg.Button('ON',size=(5, 1)),sg.Button('OFF',size=(5, 1))],
     [sg.Text('OUTPUT:', size=(15, 1)),sg.Text('N/A',key='-l_dev_output-',size=(30, 1))],
-    [sg.Text('VOLTAGE:',size=(15, 1)),sg.Slider(range=(0, 30), orientation='h', size=(15, 15), default_value=12, key='-s_dev_vset-'),sg.Slider(range=(0, 9), orientation='h', size=(10, 15), default_value=0, key='-s_dev_vsetc-'),sg.Button('SET',key='-b_dev_vset-')],
-    [sg.Text('CURRENT_MAX:',size=(15, 1)),sg.Slider(range=(0, 10), orientation='h', size=(15, 15), default_value=5, key='-s_dev_cset-'),sg.Slider(range=(0, 9), orientation='h', size=(10, 15), default_value=0, key='-s_dev_csetc-'),sg.Button('SET',key='-b_dev_cset-')],
-    [sg.Button('EXIT'),sg.Button('POLL',size=(15, 1))]
+    [sg.Text('VOLTAGE:',size=(15, 1)),sg.Slider(range=(0, 30), orientation='h', size=(15, 15), default_value=12, key='-s_dev_vset-'),sg.Slider(range=(0, 9), orientation='h', size=(10, 15), default_value=0, key='-s_dev_vsetc-'),sg.Button('SET',size=(10, 1),key='-b_dev_vset-')],
+    [sg.Text('CURRENT_MAX:',size=(15, 1)),sg.Slider(range=(0, 10), orientation='h', size=(15, 15), default_value=5, key='-s_dev_cset-'),sg.Slider(range=(0, 9), orientation='h', size=(10, 15), default_value=0, key='-s_dev_csetc-'),sg.Button('SET',size=(10, 1),key='-b_dev_cset-')],
+    [sg.Button('EXIT',size=(10, 1)),sg.Button('POLL',size=(15, 1))]
 ]
 
 class korad_gui:
