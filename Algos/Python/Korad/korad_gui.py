8b18d075780bd0ee7199e76927d5d45041dd5a40
Umwelt32 <56482176+Umwelt32@users.noreply.github.com>
2023-10-20 13:22:15 +0200

    Update korad_gui.py

--- Algos/Python/Korad/korad_gui.py
+++ Algos/Python/Korad/korad_gui.py
@@ -21,11 +21,12 @@ m_comm_dev_list = [dev[0] for dev in korad_ctl._get_serial_list()]
 m_layout = [
     [sg.Text('COM_DEV:',size=(15, 1)),sg.Combo(m_comm_dev_list,size=(15, 1),key='-c_dev-'),sg.Combo(m_comm_brates,size=(10, 1),default_value='9600', key='-c_rate-'),sg.Button('OPEN',size=(10, 1)),sg.Button('CLOSE',size=(10, 1))],
     [sg.Text('DEVICE:', size=(15, 1)),sg.Text('<NOT OPENED!>',size=(25, 1), key='-l_dev_name-')],
-    [sg.Text('STATUS:', size=(15, 1)),sg.Canvas(size=(25, 25), background_color='red', key='-l_dev_status_led-'),sg.Text('N/A',key='-l_dev_status-',size=(10, 1)),sg.Button('ON',size=(10, 1)),sg.Button('OFF',size=(10, 1))],
-    [sg.Text('OUTPUT:', size=(15, 1)),sg.Text('N/A',key='-l_dev_output-',size=(30, 1))],
+    [sg.Text('OVP/OCP:',size=(15, 1)),sg.Canvas(size=(25, 25), background_color='red', key='-l_dev_ovp_ocp_status_led-'),sg.Button('OVP',size=(5, 1)),sg.Button('OCP',size=(5, 1)),sg.Button('DISABLE',size=(10, 1))],
+    [sg.Text('STATUS:', size=(15, 1)),sg.Canvas(size=(25, 25), background_color='red', key='-l_dev_status_led-'),sg.Text('N/A',key='-l_dev_status-',size=(5, 1)),sg.Button('ON',size=(10, 1)),sg.Button('OFF',size=(10, 1))],
+    [sg.Text('OUTPUT:', size=(15, 1)),sg.Text('N/A',key='-l_dev_output-',font=('Helvetica', 25),size=(15, 1))],
     [sg.Text('VOLTAGE:',size=(15, 1)),sg.Slider(range=(0, 30), orientation='h', size=(15, 15), default_value=12, key='-s_dev_vset-'),sg.Slider(range=(0, 9), orientation='h', size=(10, 15), default_value=0, key='-s_dev_vsetc-'),sg.Button('SET',size=(10, 1),key='-b_dev_vset-')],
     [sg.Text('CURRENT_MAX:',size=(15, 1)),sg.Slider(range=(0, 10), orientation='h', size=(15, 15), default_value=5, key='-s_dev_cset-'),sg.Slider(range=(0, 9), orientation='h', size=(10, 15), default_value=0, key='-s_dev_csetc-'),sg.Button('SET',size=(10, 1),key='-b_dev_cset-')],
-    [sg.Text('OVP/OCP:',size=(15, 1)),sg.Canvas(size=(25, 25), background_color='red', key='-l_dev_ovp_ocp_status_led-'),sg.Button('OVP',size=(5, 1)),sg.Button('OCP',size=(5, 1)),sg.Button('DISABLE',size=(10, 1))],
+    [sg.Text('_' * 70, text_color='black', background_color='white',justification='center',size=(70, 1))],
     [sg.Button('EXIT',size=(10, 1)),sg.Button('POLL',size=(15, 1))]
 ]
 
@@ -114,3 +115,4 @@ def _main(debug=False):
 
 if __name__ == "__main__":
     _main()
+    
